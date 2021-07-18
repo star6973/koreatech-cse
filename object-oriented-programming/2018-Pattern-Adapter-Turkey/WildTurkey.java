25fc57afe45a744a5ab636fe5c0ee6c6ebc954ab
Ji Myung Hwa <battlesun99@gmai.com>
2021-07-18 23:50:49 +0900

    Feat: 객체지향 디자인패턴 [Adapter]

---
+++ object-oriented-programming/2018-Pattern-Adapter-Turkey/WildTurkey.java
@@ -0,0 +1,13 @@
+
+public class WildTurkey implements Turkey {
+	@Override
+	public void gobble() {
+		System.out.println("고블 고블");
+
+	}
+
+	@Override
+	public void fly() {
+		System.out.println("푸드덕 푸드덕");
+	}
+}
